76f63e79944c644c3d592c28f0d39f47315781bd
Mian Muhammad Umair Amer <m.umairamer@gmail.com>
2019-09-05 14:07:40 +0300

    After uploading file you can view file content

--- DockerComposeMVC/Controllers/HomeController.cs
+++ DockerComposeMVC/Controllers/HomeController.cs
@@ -103,10 +103,11 @@ namespace DockerComposeMVC.Controllers
             return Ok(Composer.GetStatus() + " " + Composer.GetStatus().Length);
         }
 
-        public IActionResult GetConfig() {
+        public IActionResult GetConfig()
+        {
             Configuration.ReadConfigFile();
             var config = Configuration.GetConfig();
-            var list = ComposeFileOperationsNew.GetComposeFromFile(Path.Combine(Directory.GetCurrentDirectory(), "data\ready"));
+            var list = ComposeFileOperationsNew.GetComposeFromFiles(Path.Combine(Directory.GetCurrentDirectory(), "data\ready"));
             return Ok(list);
         }
 
