b1a0d371b8525602d3db5c306b3251d9264fc087
Mian Muhammad Umair Amer <m.umairamer@gmail.com>
2019-09-09 14:56:55 +0300

    StopRunningComposeFile Works

--- DockerComposeMVC/Controllers/HomeController.cs
+++ DockerComposeMVC/Controllers/HomeController.cs
@@ -140,39 +140,33 @@ namespace DockerComposeMVC.Controllers
             return View(RunningList);
         }
 
+        public IActionResult StopRunningFile([FromQuery] String FileName)
+        {
+            try
+            {
+                string result = ComposerNew.StopService(FileName);
+                var RunningList = ComposerNew.GetRunningServices();
+                return View("RunningReadyFiles", RunningList);
+            }
+            catch (Exception e)
+            {
+                return NotFound("ERR_NO_SUCH_RUNNING_COMPOSE_FILE_FOUND: " + FileName);
+            }
+            
+        }
+
         public IActionResult StatusDebug()
         {
             return Ok(Composer.GetStatus() + " " + Composer.GetStatus().Length);
         }
-
-        public IActionResult GetConfig()
-        {
-            Configuration.ReadConfigFile();
-            var config = Configuration.GetConfig();
-
-            var list = ComposeFileOperationsNew.LoadCompositesFromFiles(Path.Combine(Directory.GetCurrentDirectory(), @"data\ready"), true);
-            return Ok(list);
-        }
-
-        public IActionResult Stop()
-        {
-            ViewData["success"] = false;
-            ViewData["message"] = "Either there was no application running or the running application was not stopped successfully";
-            if (Composer.GetStatus() == "Running" && Composer.Stop() == "Stopped")
-            {
-                ViewData["success"] = true;
-                ViewData["message"] = "The application was stopped successfully";
-            }
-            return View();
-        }
-
+        
         public IActionResult UploadCompose()
         {
             return View();
         }
-
+        
         [HttpPost]
-        public IActionResult SubmitNew([FromForm] Dictionary<string, string> dict)
+        public IActionResult CreateCustom([FromForm] Dictionary<string, string> dict, String fileName)
         {
             if (Composer.GetStatus() == "Running")
             {
@@ -181,34 +175,9 @@ namespace DockerComposeMVC.Controllers
                 ViewData["message"] = "A multi-container application is already running. Please stop it before attempting to start another one!";
                 return View();
             }
-
-            string finalComposeString = ComposeFileOperations.ReplaceParams(ComposeFileOperations.ReadFile(), dict);
-            if (ComposeFileOperations.WriteToFile(finalComposeString))
-            {
-                Composer.Run("Compose_Application");
-                Thread.Sleep(1000);
-
-                ViewData["Title"] = "Success";
-                ViewData["success"] = true;
-                ViewData["message"] = "Your application has been started. You will be redirected to the status page in a few seconds.";
-                ViewData["status"] = Composer.GetStatus();
-            }
-
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult CreateCustom([FromForm] Dictionary<string, string> dict, String fileString)
-        {
-            if (Composer.GetStatus() == "Running")
-            {
-                ViewData["Title"] = "Failure";
-                ViewData["success"] = false;
-                ViewData["message"] = "A multi-container application is already running. Please stop it before attempting to start another one!";
-                return View();
-            }
-
-            string finalComposeString = ComposeFileOperations.ReplaceParams(fileString, dict);
+            String basePath = Path.Combine(Directory.GetCurrentDirectory(), "temp/" + fileName);
+            String fileString = System.IO.File.ReadAllText(basePath);
+            string finalComposeString = ComposerNew.ReplaceParams(fileString, dict);
             if (ComposeFileOperations.WriteToFile(finalComposeString))
             {
                 Composer.Run("Compose_Application");
