68eaef3c308090ca5933553cdfbb93afc4f63d2f
Muhammad Umair Ahmed <43809813+ahmed-umair@users.noreply.github.com>
2019-09-10 11:34:46 +0300

    Added Log4Net to Startup

--- DockerComposeMVC/Startup.cs
+++ DockerComposeMVC/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace DockerComposeMVC
 {
@@ -37,7 +38,7 @@ namespace DockerComposeMVC
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
@@ -53,6 +54,7 @@ namespace DockerComposeMVC
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
+            loggerFactory.AddLog4Net();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
